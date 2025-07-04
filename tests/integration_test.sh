9dceeaa04f2da21c54e56a1bf93e88f1aebefb22
Maxime Haselbauer <renn0xtek9@laposte.net>
2025-07-04 16:05:39 +0200

    Fix integration tests
    Fix integration tests

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -3,17 +3,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/test_utils.sh"
 set -e 
 cd output
-sudo apt remove -y cmake-dependency-diagram
+sudo apt remove -y cmake-dependency-diagram || true
 
 # Ensure the package is owned by _apt user. See https://www.reddit.com/r/linux4noobs/comments/ux6cwx/installation_errors_on_deb_files_apt/
 # It enables us skipping some warning that do not return 0.
 sudo chown _apt ./cmake-dependency-diagram_1.0.0_all.deb 
+cd "$DIR"/../output
+pwd
 sudo apt install -y ./cmake-dependency-diagram_1.0.0_all.deb
-cd ..
-cd tests 
+
+cd "$DIR"/../tests 
 BUILD_DIR=./../build/integration_test
 mkdir -p "$BUILD_DIR"
-cmake -S integration_test -B "$BUILD_DIR" --graphviz="$BUILD_DIR"cmake.dot
+cmake -S integration_test -B "$BUILD_DIR" --graphviz="$BUILD_DIR"/cmake.dot
 cmake --build "$BUILD_DIR" --target cmake-dependency-diagrams
 
 
