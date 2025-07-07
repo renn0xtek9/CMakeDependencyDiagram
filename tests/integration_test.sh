21e15d20caa632e293972e9a0e125a880cc6ddc4
Maxime Haselbauer <renn0xtek9@laposte.net>
2025-07-07 08:09:27 +0200

    Fix missing sources in PPA Package (#3)
    * Refactor Makefile and fix package not being uploaded properly to ppa
    
    * Fix no build directory after clean when uninstalling
    
    * Adapt README.md
    
    * Remove outdated script

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -2,11 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GREEN="\033[0;32m"
 NC="\033[0m"
+
+PLATFORM="$1" # e.g., "ubuntu-22" or "ubuntu-24"
+
 source "$DIR/test_utils.sh"
 set -e 
 
 cd "$DIR"/../
-BUILD_DIR=./build/integration_test
+BUILD_DIR=./build/integration_test/"$PLATFORM"
 
 expect_file "$BUILD_DIR"/CMakeDependencyDiagram/cmake.dot.png
 expect_file "$BUILD_DIR"/CMakeDependencyDiagram/index.html 
